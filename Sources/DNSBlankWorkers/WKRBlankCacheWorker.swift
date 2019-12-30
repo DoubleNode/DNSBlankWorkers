2c5313a44f38c51f933803134f1011fe30679265
Darren Ehlers <me@darrenehlers.com>
2019-12-29 18:50:58 -0600

    Added UIWindowSceneDelegate methods

--- Sources/DNSBlankWorkers/WKRBlankCacheWorker.swift
+++ Sources/DNSBlankWorkers/WKRBlankCacheWorker.swift
@@ -31,6 +31,29 @@ open class WKRBlankCacheWorker: PTCLCache_Protocol
         nextWorker?.disableOption(option: option)
     }
 
+    // MARK: - UIWindowSceneDelegate methods
+
+    // Called when the scene has moved from an inactive state to an active state.
+    // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    public func didBecomeActive() {
+    }
+
+    // Called when the scene will move from an active state to an inactive state.
+    // This may occur due to temporary interruptions (ex. an incoming phone call).
+    public func willResignActive() {
+    }
+
+    // Called as the scene transitions from the background to the foreground.
+    // Use this method to undo the changes made on entering the background.
+    public func willEnterForeground() {
+    }
+
+    // Called as the scene transitions from the foreground to the background.
+    // Use this method to save data, release shared resources, and store enough scene-specific state information
+    // to restore the scene back to its current state.
+    public func didEnterBackground() {
+    }
+
     // MARK: - Business Logic / Single Item CRUD
     open func doDeleteObject(for id: String,
                              with progress: PTCLProgressBlock?,
