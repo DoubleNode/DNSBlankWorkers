24cb9689cf45b258a00a1a3cfa0989a864d7a6f2
Darren Ehlers <me@darrenehlers.com>
2020-01-10 11:50:24 -0600

    Changed access level of UIWindowSceneDelegate methods

--- Sources/DNSBlankWorkers/WKRBlankBeaconsWorker.swift
+++ Sources/DNSBlankWorkers/WKRBlankBeaconsWorker.swift
@@ -37,23 +37,23 @@ open class WKRBlankBeaconsWorker: NSObject, PTCLBeacons_Protocol
 
     // Called when the scene has moved from an inactive state to an active state.
     // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    public func didBecomeActive() {
+    open func didBecomeActive() {
     }
 
     // Called when the scene will move from an active state to an inactive state.
     // This may occur due to temporary interruptions (ex. an incoming phone call).
-    public func willResignActive() {
+    open func willResignActive() {
     }
 
     // Called as the scene transitions from the background to the foreground.
     // Use this method to undo the changes made on entering the background.
-    public func willEnterForeground() {
+    open func willEnterForeground() {
     }
 
     // Called as the scene transitions from the foreground to the background.
     // Use this method to save data, release shared resources, and store enough scene-specific state information
     // to restore the scene back to its current state.
-    public func didEnterBackground() {
+    open func didEnterBackground() {
     }
 
     // MARK: - Business Logic / Single Item CRUD
