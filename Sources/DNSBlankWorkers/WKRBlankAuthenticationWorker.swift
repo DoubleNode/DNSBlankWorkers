7530eebea43077744ab7f6f234bd2d244967be53
Darren Ehlers <me@darrenehlers.com>
2020-02-17 15:00:51 -0600

    Added WKRBlankAuthenticationWorker

---
+++ Sources/DNSBlankWorkers/WKRBlankAuthenticationWorker.swift
@@ -0,0 +1,74 @@
+//
+//  WKRBlankAuthenticationWorker.swift
+//  DoubleNode Core - DNSBlankWorkers
+//
+//  Created by Darren Ehlers on 2019/08/12.
+//  Copyright © 2019 - 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//
+
+import DNSProtocols
+import Foundation
+
+open class WKRBlankAuthenticationWorker: WKRBlankBaseWorker, PTCLAuthentication_Protocol
+{
+    public var nextWorker: PTCLAuthentication_Protocol?
+
+    public required init() {
+        super.init()
+    }
+
+    public required init(nextWorker: PTCLAuthentication_Protocol) {
+        super.init()
+        self.nextWorker = nextWorker
+    }
+
+    override open func enableOption(option: String) {
+        nextWorker?.enableOption(option: option)
+    }
+
+    override open func disableOption(option: String) {
+        nextWorker?.disableOption(option: option)
+    }
+
+    // MARK: - Business Logic / Single Item CRUD
+
+    open func doCheckAuthentication(using parameters: [String: Any],
+                                    with progress: PTCLProgressBlock?,
+                                    and block: PTCLAuthenticationBlockVoidBoolAccessDataError) throws {
+        guard nextWorker != nil else {
+            return
+        }
+
+        try nextWorker!.doCheckAuthentication(using: parameters,
+                                              with: progress,
+                                              and: block)
+    }
+
+    open func doSignIn(from username: String?,
+                       and password: String?,
+                       using parameters: [String: Any],
+                       with progress: PTCLProgressBlock?,
+                       and block: @escaping PTCLAuthenticationBlockVoidBoolAccessDataError) throws {
+        guard nextWorker != nil else {
+            return
+        }
+
+        try nextWorker!.doSignIn(from: username,
+                                 and: password,
+                                 using: parameters,
+                                 with: progress,
+                                 and: block)
+    }
+
+    open func doSignOut(using parameters: [String: Any],
+                        with progress: PTCLProgressBlock?,
+                        and block: @escaping PTCLAuthenticationBlockVoidBoolError) throws {
+        guard nextWorker != nil else {
+            return
+        }
+
+        try nextWorker!.doSignOut(using: parameters,
+                                  with: progress,
+                                  and: block)
+    }
+}
