59802e8b83e771cf18ada89c0a74560e704bdb18
Darren Ehlers <me@darrenehlers.com>
2024-06-05 21:16:02 -0500

    Updated package dependencies

--- Package.swift
+++ Package.swift
@@ -27,11 +27,11 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.1"),
-        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.11.5"),
-        .package(url: "https://github.com/DoubleNode/DNSCrashNetwork.git", from: "1.11.1"),
-        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.11.6"),
-        .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.11.0"),
-        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.11.9"),
+        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.11.6"),
+        .package(url: "https://github.com/DoubleNode/DNSCrashNetwork.git", from: "1.11.2"),
+        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.11.8"),
+        .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.11.1"),
+        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.11.10"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
